9efdef20a1621eb0a40c8442be8423991b0ba8ee
291678890 <55072618+291678890@users.noreply.github.com>
2019-12-03 19:06:11 +0800

    影片出租程序第二次上传，第一次忘了加上Main了

---
+++ 影片出租程序/src/影片出租程序/Main.java
@@ -0,0 +1,9 @@
+package Ó°Æ¬³ö×â³ÌÐò;
+
+public class Main{
+	  public static void main(String[] args) {
+	      Customer c1=new Customer("zhangsan");
+	      c1. addRental(new Rental(new Movie("lisi",2),6));
+	      System. out.print(c1. statement());
+	  }
+	}
--- 影片出租程序/src/影片出租程序/Movie.java
+++ 影片出租程序/src/影片出租程序/Movie.java
@@ -1,5 +1,24 @@
 package Ó°Æ¬³ö×â³ÌÐò;
 
 public class Movie {
-
+	public static final int CHILDRENS=2;
+	public static final int REGULAR=0;
+	public static final int NEW_RELEASE=1;
+	
+	private String _title;
+	private int _priceCode;
+	
+	public Movie(String title,int priceCode) {
+		_title=title;
+		_priceCode=priceCode;
+	}
+	public int getPriceCode() {
+		return _priceCode;
+	}
+	public void setPriceCode(int arg) {
+		_priceCode=arg;
+	}
+	public String getTitle() {
+		return _title;
+	}
 }
